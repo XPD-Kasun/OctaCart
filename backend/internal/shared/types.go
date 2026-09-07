2f1e3a6fd11c551e2e9e399f94ea3c95ad51f797
XPDKasun <Kasunx.kelaniya@gmail.com>
2026-09-07 14:55:22 +0530

    feat(product): Adds base types

--- backend/internal/shared/types.go
+++ backend/internal/shared/types.go
@@ -1,5 +1,7 @@
 package shared
 
+import "time"
+
 // The type of a claim is string. i.e. A claim is described as a string
 type Claim string
 
@@ -12,4 +14,22 @@ type Money int64
 type KV[Key comparable, Val any] struct {
 	Key Key
 	Val Val
+}
+
+// Represent Id of any user that is capable of configuring admin. Basically, any staff users merchant creates or implicit admin for system tasks. These do not include any customer ids.
+type AdminId int
+
+type Pagination struct {
+	Page    int
+	PerPage int
+}
+
+func (p Pagination) Offset() int {
+	return (p.Page - 1) * p.PerPage
+}
+
+type DomainEvent interface {
+	EventName() string
+	OccurredAt() time.Time
+	ActorId() UserId
 }
